929d3d5a52dfe49a2b669dd4fd9447228aada0ef
Macbook <macbook@Macbooks-MacBook-Pro-MDL.local>
2022-01-16 16:51:23 -0400

    First commit

--- src/main.py
+++ src/main.py
@@ -34,7 +34,7 @@ def sitemap():
 def handle_hello():
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "msg": "Wow, this is your GET /user response "
     }
 
     return jsonify(response_body), 200
