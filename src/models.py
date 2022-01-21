6bb948a971324f7dc9a05f12f3e3d98abfea3377
dmilanop <dmilanop28@gmail.com>
2022-01-21 17:20:38 +0000

    Tablas creadas en la base de datos, falta relacionarlas :)

--- src/models.py
+++ src/models.py
@@ -7,9 +7,6 @@ class User(db.Model):
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(100), unique=False, nullable=False)
 
-    # def __repr__(self):
-    #     return '<User %r>' % self.username
-
     # def serialize(self):
     #     return {
     #         "id": self.id,
@@ -17,5 +14,39 @@ class User(db.Model):
     #         # do not serialize the password, its a security breach
     #     }
 
-# class User_information(db.Model): 
-#     name = db.Column(db.String(80))
+class User_recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    # recipes_id = db.Column(db.Integer, db.ForeignKey('recipes.id'), nullable=False)
+
+class Recipes(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipes_name = db.Column(db.String(150), nullable=False, unique=True)
+    description = db.Column(db.String(250), nullable=False, unique=True)
+
+class User_information(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    # user_id = db.Column(db.Integer, ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=False)
+    last_name = db.Column(db.String(150), nullable=False, unique=False)
+    gender = db.Column(db.String(80), nullable=False, unique=False)
+    date_of_birth =db.Column(db.Integer, nullable=False, unique=False)
+    height = db.Column(db.Float, nullable=False, unique=False)
+    weight = db.Column(db.Float, nullable=False, unique=False)
+    ailments = db.Column(db.Boolean, nullable=False, unique=False)
+    exercises = db.Column(db.Boolean, nullable=False, unique=False)
+
+class Medical_history(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    diabetes = db.Column(db.Boolean, nullable=False, unique=False)
+    uric_acid = db.Column(db.Boolean, nullable=False, unique=False)
+    gastric_ulcers = db.Column(db.Boolean, nullable=False, unique=False)
+    gastritis = db.Column(db.Boolean, nullable=False, unique=False)
+    cholesterol = db.Column(db.Boolean, nullable=False, unique=False)
+    triglycerides = db.Column(db.Boolean, nullable=False, unique=False)
+    dairy_intolerance = db.Column(db.Boolean, nullable=False, unique=False)
+    celiac = db.Column(db.Boolean, nullable=False, unique=False)
+    obesity = db.Column(db.Boolean, nullable=False, unique=False)
+    kidney_stones = db.Column(db.Boolean, nullable=False, unique=False)
+    inflametion_of_the_colon = db.Column(db.Boolean, nullable=False, unique=False)
+    heart_problems = db.Column(db.Boolean, nullable=False, unique=False)
